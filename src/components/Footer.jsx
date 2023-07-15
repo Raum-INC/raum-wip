1b135f659153d81be9e11b1514415cef72280fa4
Emmanuel Inua <artofnuel@gmail.com>
2023-07-15 09:44:07 +0100

    Included Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import logo from "../assets/raum_logo.png";
+import { siteLinks } from "./links";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full md:max-w-[1267px] mx-auto flex flex-col md:flex-row justify-between gap-y-6 p-8">
+      <div>
+        <img src={logo} alt="" />
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-y-8">
+        {siteLinks.map((siteLink, index) => (
+          <div
+            key={index}
+            className="md:w-[205px] flex flex-col gap-6 md:gap-10"
+          >
+            <h4 className="font-bold text-xl text-[#767676] leading-6">
+              {siteLink.title}
+            </h4>
+            <div className="flex flex-col gap-3 md:gap-10">
+              {siteLink.links.map((link, index) => (
+                <a
+                  href="#"
+                  key={index}
+                  className="font-normal text-base leading-6"
+                >
+                  {link}
+                </a>
+              ))}
+            </div>
+          </div>
+        ))}
+        {/* <div className="md:w-[205px]">
+          Products
+          <div>
+            <a href="#">Raum Bue</a>
+          </div>
+          <div>
+            <a href="#">Raum Bue</a>
+          </div>
+          <div>
+            <a href="#">Raum Bue</a>
+          </div>
+          <div>
+            <a href="#">Raum Bue</a>
+          </div>
+        </div> */}
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
---
+++ src/components/links.js
@@ -0,0 +1,18 @@
+export const siteLinks = [
+  {
+    title: "Products",
+    links: ["Raum Bue", "Earn More", "Mobile App", "Web app"],
+  },
+  {
+    title: "Contact Us",
+    links: ["Raumgroup@gmail.com", "Phone number", "Twitter", "Linkedin"],
+  },
+  {
+    title: "Support",
+    links: ["Pricing", "Documentation", "Customer care"],
+  },
+  {
+    title: "Company",
+    links: ["About us", "FAQ"],
+  },
+];
